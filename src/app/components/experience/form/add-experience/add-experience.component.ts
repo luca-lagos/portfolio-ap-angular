e8706fdd97b8dff07c7abecf6ac1b4ac70d02fc2
luca-lagos <lagos.herrera.luca@gmail.com>
2022-08-10 23:59:54 -0300

    Upload experience component

--- src/app/components/experience/form/add-experience/add-experience.component.ts
+++ src/app/components/experience/form/add-experience/add-experience.component.ts
@@ -1,7 +1,7 @@
 import { Router } from '@angular/router';
 import { ExperienceService } from './../../../../services/experience.service';
 import { Experience } from 'src/app/models/experience';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -20,15 +20,35 @@ export class AddExperienceComponent implements OnInit {
   workStart: Date = new Date();
   workEnd: Date = new Date();
   actualWork: boolean = false;
+  workTimeList: string[] = [
+    'Jornada parcial',
+    'Jornada completa',
+    'Contrato',
+    'Pasantías',
+  ];
+  actualWorkList: any[] = [
+    {
+      text: 'Si',
+      value: true,
+    },
+    {
+      text: 'No',
+      value: false,
+    },
+  ];
+  workEndState: boolean = false;
 
   constructor(
     private experienceService: ExperienceService,
-    private router: Router
+    private router: Router,
   ) {}
 
   ngOnInit(): void {}
 
   onAdd(): void {
+    if (this.actualWork === true) {
+      this.workEnd = new Date('0000-00-00 00:00:00.000000');
+    }
     const experience = new Experience(
       this.jobName,
       this.jobDescription,
@@ -41,33 +61,72 @@ export class AddExperienceComponent implements OnInit {
       this.workEnd,
       this.actualWork
     );
-    this.experienceService.save(experience).subscribe({
-      next: (data) => {
-        Swal.fire({
-          title: 'Success',
-          text: 'La experiencia se ha añadido exitosamente',
-          icon: 'success',
-          iconColor: '#20c997',
-          showConfirmButton: true,
-          confirmButtonText: 'OK',
-          confirmButtonColor: '#20c997',
-        }).then(() => {
-          this.router.navigate(['']);
-        });
-      },
-      error: (err) => {
-        Swal.fire({
-          title: 'Error',
-          text: 'Hubo un error al añadir la experiencia',
-          icon: 'error',
-          iconColor: '#dc3545',
-          showConfirmButton: true,
-          confirmButtonText: 'OK',
-          confirmButtonColor: '#dc3545',
-        }).then(() => {
-          this.router.navigate(['']);
-        });
-      },
-    });
+    if (this.workStart !== this.workEnd) {
+      console.log(
+        this.jobName,
+        this.jobDescription,
+        this.businessName,
+        this.businessImg,
+        this.location,
+        this.countryLocation,
+        this.workTime,
+        this.workStart,
+        this.workEnd,
+        this.actualWork
+      );
+      this.experienceService.save(experience).subscribe({
+        next: (data) => {
+          Swal.fire({
+            title: 'Success',
+            text: 'La experiencia se ha añadido exitosamente',
+            icon: 'success',
+            iconColor: '#20c997',
+            showConfirmButton: true,
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#20c997',
+          }).then(() => {
+            this.router.navigate(['']);
+          });
+        },
+        error: (err) => {
+          Swal.fire({
+            title: 'Error',
+            text: 'Hubo un error al añadir la experiencia',
+            icon: 'error',
+            iconColor: '#dc3545',
+            showConfirmButton: true,
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#dc3545',
+          }).then(() => {
+            this.router.navigate(['/add-experience']);
+          });
+        },
+      });
+    } else {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 2500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer);
+          toast.addEventListener('mouseleave', Swal.resumeTimer);
+        },
+      });
+      Toast.fire({
+        title: 'Las fechas de inicio y final no deben ser iguales',
+        icon: 'warning',
+        iconColor: '#f1c40f',
+      });
+    }
+  }
+
+  onSelectActualWork(): void {
+    if (this.actualWork === true) {
+      this.workEndState = true;
+    } else {
+      this.workEndState = false;
+    }
   }
 }
--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -10,25 +10,25 @@ export class ExperienceService {
   experienceURL: string = 'http://localhost:8080/experience';
   constructor(private httpClient: HttpClient) {}
   public list(): Observable<Experience[]> {
-    return this.httpClient.get<Experience[]>(this.experienceURL + 'list');
+    return this.httpClient.get<Experience[]>(this.experienceURL + '/list');
   }
 
   public detail(id: number): Observable<Experience> {
-    return this.httpClient.get<Experience>(this.experienceURL + `detail/${id}`);
+    return this.httpClient.get<Experience>(this.experienceURL + `/detail/${id}`);
   }
 
   public save(experience: Experience): Observable<any> {
-    return this.httpClient.post<any>(this.experienceURL + 'create', experience);
+    return this.httpClient.post<any>(this.experienceURL + '/add', experience);
   }
 
   public update(id: number, experience: Experience): Observable<any> {
     return this.httpClient.put<any>(
-      this.experienceURL + `update/${id}`,
+      this.experienceURL + `/edit/${id}`,
       experience
     );
   }
 
   public delete(id: number): Observable<any> {
-    return this.httpClient.delete<any>(this.experienceURL + `delete/${id}`);
+    return this.httpClient.delete<any>(this.experienceURL + `/delete/${id}`);
   }
 }
